3624ddc1cba2f19cbbfe79e3a880f54820248b21
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-09-19 15:19:03 +0700

    first commit

---
+++ routes/routes.go
@@ -0,0 +1,30 @@
+package routes
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ilhamadikusuma31/golang-ecommerce/controllers"
+	"github.com/ilhamadikusuma31/golang-ecommerce/database"
+	"github.com/ilhamadikusuma31/golang-ecommerce/middleware"
+	"github.com/ilhamadikusuma31/golang-ecommerce/routes"
+)
+
+func main() {
+
+	port := "8080"
+	app := controllers.NewApplication(database.ProductData(database.client, "Products"), database.UserData(database.client, "Users"))
+
+	router := gin.New()
+	routes.UserRoutes(router)
+	routes.Use(middleware.Autentikasi())
+
+	router.GET("/tambah-ke-keranjang", app.TambahKeKeranjang())
+	router.GET("/hapus-item", app.HapusItem())
+	router.GET("/keranjang-checkout", app.KeranjangCheckout())
+	router.GET("/beli-cepat", app.BeliCepat())
+	router.GET("/beli-cepat", app.BeliCepat())
+
+	//run
+	log.Fatal(router.Run(":"+port))
+}
