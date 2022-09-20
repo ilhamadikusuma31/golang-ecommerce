4935d22393295eaa3bd210da43aebdf3f3084291
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-09-20 16:27:06 +0700

    databasetup.go selesai

---
+++ database/cart.go
@@ -0,0 +1,29 @@
+package database
+
+import "errors"
+
+var (
+	ErrCantFindTheProduct = errors.New("ga nemu produknya")
+	ErrCantDecodeTheProducts = errors.New("ga nemu produk2nya")
+	ErrUserIdIsNotValid = errors.New("user id nya ga valid")
+	ErrCantUpdateUser = errors.New("ga bisa update user")
+	ErrCantRemoveItemCart = errors.New("ga bisa ngapus item dari keranjang")
+	ErrCantGetItem = errors.New("ga bisa dapet item dari keranjang")
+	ErrCantBuyCartItem= errors.New("ga bisa beli item yang ada di keranjang")
+)
+
+func TambahProdukKeKeranjang()  {
+	
+}
+
+func HapusProdukDariKeranjang()  {
+	
+
+}
+func BeliProdukDariKeranjang()  {
+	
+}
+
+func PembeliCepat()  {
+	
+}
--- routes/routes.go
+++ routes/routes.go
@@ -13,7 +13,7 @@ import (
 func main() {
 
 	port := "8080"
-	app := controllers.NewApplication(database.ProductData(database.client, "Products"), database.UserData(database.client, "Users"))
+	app := controllers.NewApplication(database.ProdukData(database.client, "Products"), database.UserData(database.client, "Users"))
 
 	router := gin.New()
 	routes.UserRoutes(router)
