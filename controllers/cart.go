0a33f2e00ded4ecc7df821192cd7bb615b206593
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-09-28 16:55:17 +0700

    method di cart.go donne kecuali getItemFromCart

--- controllers/cart.go
+++ controllers/cart.go
@@ -35,8 +35,8 @@ func (a *Aplikasi) TambahKeKeranjang() gin.HandlerFunc{
 			return
 		}
 		
-		userID := c.Query("userID")
-		if userID == " "{
+		userQueryID := c.Query("userID")
+		if userQueryID == " "{
 			log.Println("id user gada")
 			_ = c.AbortWithError(http.StatusBadRequest, errors.New("id user gada"))
 			return
@@ -49,11 +49,10 @@ func (a *Aplikasi) TambahKeKeranjang() gin.HandlerFunc{
 			c.AbortWithStatus(http.StatusInternalServerError)
 			return
 		}
-
 		var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 
-		err = database.TambahProdukKeKeranjang(ctx, a.produkKoleksi, a.penggunaKoleksi, productID, userID)
+		err = database.TambahProdukKeKeranjang(ctx, a.produkKoleksi, a.penggunaKoleksi, productID, userQueryID)
 		if err!=nil{
 			c.JSON(http.StatusInternalServerError, err)
 		}
@@ -64,15 +63,100 @@ func (a *Aplikasi) TambahKeKeranjang() gin.HandlerFunc{
 }
 
 
-func HapusItem() gin.HandlerFunc{
+func (a *Aplikasi) HapusItem() gin.HandlerFunc{
+	return func (c *gin.Context)  {
+		produkQueryId := c.Query("produkID")
+		if produkQueryId == " "{
+			log.Println("id produk gada")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("id produk gada"))
+			return
+		}
+		
+		userQueryID := c.Query("userID")
+		if userQueryID == " "{
+			log.Println("id user gada")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("id user gada"))
+			return
+		}
 
+		//bikin id baru
+		productID, err := primitive.ObjectIDFromHex(produkQueryId)
+		if err!=nil{
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		err = database.HapusProdukDariKeranjang(ctx, a.produkKoleksi, a.penggunaKoleksi, productID, userQueryID)
+
+		if err != nil{
+			c.JSON(http.StatusInternalServerError,err)
+			return
+		}
+
+		c.JSON(200,"berhasil menghapus item")
+
+	}
 }
 func DapatkanItemDariKeranjang() gin.HandlerFunc{
 
 }
 func BeliDariKeranjang() gin.HandlerFunc{
+	return func (c *gin.Context)  {
+		userQueryID := c.Query("userID")
+		if userQueryID == " "{
+			log.Println("id user gada")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("id user gada"))
+			return
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		err := database.BeliProdukDariKeranjang(ctx, a.penggunaKoleksi, userQueryID)
+		if err != nil{
+			c.JSON(http.StatusInternalServerError, err)
+		}
+
+		c.JSON(200,"sukses menyiapkan pesanan")
+
+	}
 }
 func BeliCepat() gin.HandlerFunc{
+	return func (c *gin.Context)  {
+		produkQueryId := c.Query("produkID")
+		if produkQueryId == " "{
+			log.Println("id produk gada")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("id produk gada"))
+			return
+		}
+		
+		userQueryID := c.Query("userID")
+		if userQueryID == " "{
+			log.Println("id user gada")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("id user gada"))
+			return
+		}
 
+		//bikin id baru
+		productID, err := primitive.ObjectIDFromHex(produkQueryId)
+		if err!=nil{
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		err = database.PembeliCepat(ctx, a.produkKoleksi, a.penggunaKoleksi, productID, userQueryID)
+
+		if err != nil{
+			c.JSON(http.StatusInternalServerError,err)
+			return
+		}
+
+		c.JSON(200,"berhasil menambahkan pesanan")
+
+	}
 }
