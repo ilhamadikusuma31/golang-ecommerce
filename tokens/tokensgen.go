ef199c9bd42ed1d7e27c2d035317514fe65137ab
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-10-30 17:29:21 +0700

    ValidateToken & UpdateAllTokens di token.go done

--- tokens/tokensgen.go
+++ tokens/tokensgen.go
@@ -1,13 +1,17 @@
 package tokens
 
 import (
+	"context"
 	"log"
 	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/ilhamadikusuma31/golang-ecommerce/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type SignedDetails struct {
@@ -54,10 +58,54 @@ func TokenGenerator(email string, namadepan string, namabelakang string, uid str
 
 }
 
-func ValidateToken(){
+func ValidateToken(signedtoken string) (klaims *SignedDetails, pesan string){
+	token, err:= jwt.ParseWithClaims(signedtoken, &SignedDetails{}, func(token *jwt.Token)(interface{}, error){
+		return []byte(SECRET_KEY), nil
+	})
 
+	if err != nil {
+		pesan = err.Error()
+		return
+	}
+
+	claims, ok := token.Claims.(*SignedDetails)
+	if !ok{
+		pesan = "token ga valid"
+		return
+	}
+
+	if claims.ExpiresAt < time.Now().Local().Unix(){
+		pesan = "token dah expired"
+		return
+	}
+
+	return claims, pesan	
 }
+func UpdateAllTokens(signedtoken string, signedrefreshtoken string, userid string){
 
-func UpdateAllTokens(){
+	var ctx, cancel = context.WithTimeout(context.Background(),100*time.Second)
+	
+	var updateObj primitive.D
+	updateObj = append(updateObj, bson.E{Key:"token", Value:signedtoken})
+	updateObj = append(updateObj, bson.E{Key:"refresh_token", Value:signedrefreshtoken})
+	updated_at, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	updateObj = append(updateObj, bson.E{Key:"updatedat", Value: updated_at})
+
+	upsert := true
+	filter := bson.M{"user_id":userid}
+
+	opt := options.UpdateOptions{    //kalo ga ada data, tambahkan
+		Upsert : &upsert,
+	}	
+
+	_,err :=  UserData.UpdateOne(ctx, filter, bson.D{
+		{Key: "$set", Value:updateObj},
+	}, &opt)
+
+	defer cancel()
+	if err!=nil{
+		log.Panic(err)
+		return 
+	}
 
 }
