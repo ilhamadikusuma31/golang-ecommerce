5e95ef29b7d0aa98fa90b018b3760749479660cf
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-10-31 15:52:13 +0700

    tambah hapus item di database/cart.go done

--- database/cart.go
+++ database/cart.go
@@ -1,7 +1,14 @@
 package database
 
 import (
+	"context"
 	"errors"
+	"log"
+
+	"github.com/ilhamadikusuma31/golang-ecommerce/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var (
@@ -15,12 +22,54 @@ var (
 )
 
 
-func TambahProdukKeKeranjang()  {
-	
+func TambahProdukKeKeranjang(ctx context.Context, prodKoleksi, userKoleksi *mongo.Collection, produkId primitive.ObjectID, userID string) error {
+	cariDiDB, err := prodKoleksi.Find(ctx, bson.M{"_id": userID})
+	if err!=nil {
+		log.Println(err)
+		return ErrCantFindTheProduct
+	}
+
+	var productCart []models.ProdukUser
+	err = cariDiDB.All(ctx, &productCart)
+	if err!=nil {
+		log.Println(err)
+		return ErrCantDecodeTheProducts
+	}
+
+	// id produk berbentuk objek id mirip punya mongo
+	// sedangkan id user masih berbentuk string
+	id, err := primitive.ObjectIDFromHex(userID)
+	if err!=nil {
+		log.Println(err)
+		return ErrUserIdIsNotValid
+	}
+
+	filter:= bson.D{primitive.E{Key: "_id", Value:id}}
+	update:= bson.D{primitive.E{Key:"$push", Value:bson.D{primitive.E{Key: "usercart", Value:productCart}}}}
+
+	_, err = userKoleksi.UpdateOne(ctx, filter, update)
+	if err!=nil {
+		return ErrCantUpdateUser
+	} 
+
+	return nil
 }
 
-func HapusProdukDariKeranjang()  {
-	
+func HapusProdukDariKeranjang(ctx context.Context, prodKoleksi, userKoleksi *mongo.Collection, produkId primitive.ObjectID, userID string) error  {
+	id, err := primitive.ObjectIDFromHex(userID)
+	if err!=nil {
+		log.Println(err)
+		return ErrUserIdIsNotValid
+	}
+	filter:= bson.D{primitive.E{Key: "_id", Value:id}}
+	update:= bson.M{"$pull": bson.M{"usercart":bson.M{"_id":produkId}}}
+
+	_, err = userKoleksi.UpdateMany(ctx, filter, update)
+	if err!=nil {
+		return ErrCantRemoveItemCart
+	} 
+
+	return nil
 
 }
 func BeliProdukDariKeranjang()  {
